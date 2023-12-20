18054fe8eb00592ac68a8d485016816d0e4df94c
Carlos Bolanos <cbolanos@expediagroup.com>
2023-12-19 16:40:58 -0800

    add script and yeoman generator

---
+++ packages/react-component-generator/generators/app/templates/src/index.ejs.js
@@ -0,0 +1,1 @@
+export * from './react-<%=componentName%>.tsx'
--- packages/scripts/publishing/publish.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "┏━━━  publish: $(pwd) ━━━━━━━━━━━━━━━━━━━"
-
-#git push origin $(git rev-parse --abbrev-ref HEAD)
